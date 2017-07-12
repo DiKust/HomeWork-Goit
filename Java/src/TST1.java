f91070cb154a77cb2d6cc7a736ac073fa7a95e8c
DiKust <dmitriyiykust@gmail.com>
2017-07-12 23:08:54 +0300

    Д/з 4 Задача 1

---
+++ Java/src/TST1.java
@@ -0,0 +1,16 @@
+
+public class TST1 {
+    public static void main(String[] args) {
+        int[] cats = new int[5];
+
+        for(int i = 0; i < cats.length; i++){
+            cats[i] = i;
+        }
+
+            for(int cat : cats){
+
+            System.out.println(cat);}
+
+
+    }
+}
