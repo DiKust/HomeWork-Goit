af156cef9881b2cad10907a22f11a75665b003ca
Kust Dima <dmitriyikust@gmail.com>
2017-07-20 20:53:09 +0300

    HomeWork

---
+++ Java/src/CarDoor/BMW.java
@@ -0,0 +1,20 @@
+package CarDoor;
+
+public class BMW {
+    private int Door;
+    private int Window;
+
+
+    public BMW() {
+
+    }
+
+    public void setDoor(int Door){
+        this.Door = Door;
+    }
+
+    public int getDoor(){
+        return Door;
+    }
+
+}
---
+++ Java/src/Homework5.java
@@ -0,0 +1,8 @@
+import CarDoor.BMW;
+
+public class Homework5 {
+    public static void main(String[] args) {
+        BMW bmw = new BMW();
+        bmw.getDoor();
+    }
+}
