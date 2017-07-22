f24b8f37afa292b36e1c35855fd7c893ff500d4f
Kust Dima <dmitriyikust@gmail.com>
2017-07-22 23:38:32 +0300

    добавил 4 домашнее задание задачи: 1, 2

---
+++ Java/src/HomeWork4/Zadacha1.java
@@ -0,0 +1,13 @@
+package HomeWork4;
+
+public class Zadacha1 {
+    public  static  void printNumber( int x ){
+        for(int i = 1; i <= x; i++){
+            System.out.print(i);
+        }
+    }
+
+    public static void main(String[] arsg){
+        printNumber(7);
+    }
+}
